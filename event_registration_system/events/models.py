b0da050b971bfaecdc07668b2c610ebc7a424c7d
Natnael Seifu <natiseifu02@gmail.com>
2024-12-24 09:54:16 +0200

    feat: Define models for Event and Registration, apply migrations

--- event_registration_system/events/models.py
+++ event_registration_system/events/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    location = models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return self.name
+
+class Registeration(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+    email = models.EmailField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.event.name}"
+    
