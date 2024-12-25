f4114cae7fe89d418a259c74370eba06c46a1a6b
Natnael Seifu <natiseifu02@gmail.com>
2024-12-25 21:46:51 +0200

    feat: add simple base and event templates, update views

--- event_registration_system/events/views.py
+++ event_registration_system/events/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def events(request):
-    return HttpResponse('<h1>hello events here</h1.')
+    return render(request, 'event/events.html')
 
 def register(request):
-    return HttpResponse('<h1>hello register here</h1.')
+    return render(request, 'event/register.html')
