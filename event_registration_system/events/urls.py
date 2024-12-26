b59687fba3d9c1d4da6e2fec757e4f179c96d770
Natnael Seifu <natiseifu02@gmail.com>
2024-12-26 21:44:26 +0200

    feat: enhance event listing and details functionality, add event details template

--- event_registration_system/events/urls.py
+++ event_registration_system/events/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.events, name='events'),
     path('register/', views.register, name='register'),
-    
+   path('event/<int:id>', views.event_details, name='event_details'),
 ]
--- event_registration_system/events/views.py
+++ event_registration_system/events/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Event, Registration
 
 def events(request):
-    return render(request, 'event/events.html')
+    events = Event.objects.all()
+    return render(request, 'event/events.html', {'events': events})
 
+def event_details(request, id):
+    event = get_object_or_404(Event, id=id)
+    return render(request, 'event/event_details.html', {'event': event})
 def register(request):
     return render(request, 'event/register.html')
